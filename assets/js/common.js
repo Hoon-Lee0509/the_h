59382e4705ec186278301f87d2b0c2e0517fb01f
Hoon Lee <wsea4d@daum.net>
2021-07-02 08:29:11 +0900

    2021.07.02 08:29
    2021.07.02 08:29

--- assets/js/common.js
+++ assets/js/common.js
@@ -36,4 +36,34 @@ $(document).ready(function () {
         if (scrollY >= 20) $('#header').addClass('on');
     });
 
+    $('#gnb > ul > li > a').on('click', function () {
+        if ($(this).next().length === 0) {  //뎁스1a 만 있는 경우
+          return true;
+        } else { //뎁스2 ul까지 있는 경우
+          // 열려질 ul의 높이를 스크립트로 지정하여 변수 설정 : li의 높이(margin포함), li의 개수 => li의 높이 x  li의 개수 = ul 높이를 알수 있다
+          const liHei = $(this).next().children().outerHeight(true);
+          const liSize = $(this).next().children().length;
+          const ulHei = liHei * liSize;
+          console.log(liHei, liSize, ulHei);
+
+          // 초기화 추가 설정: 미리 열려진 다른 메뉴 닫아주기
+          $(this).parent().siblings().removeClass('on').children('ul').stop().animate({maxHeight: 0}, function () {
+            $(this).css({visibility: 'hidden'});
+          });
+
+          if ($(this).parent().hasClass('on')) { //현재 클릭해서 열려져 있는 경우
+            $(this).parent().removeClass('on').children('ul').stop().animate({maxHeight: 0}, function () {
+              $(this).css({visibility: 'hidden'});
+            });
+            
+            /* $(this).next().stop().animate({maxHeight: 0}, function () {
+              $(this).css({visibility: 'hidden'});
+            }).parent().removeClass('on'); */
+          } else { //열려져 있지 않은 경우: ul -> visibility -> animate() -> 부모li.on
+            $(this).next().css({visibility: 'visible'}).stop().animate({maxHeight: ulHei}).parent().addClass('on');
+
+          }
+        }
+        return false;
+      });
 });
